af48a9620e5026ae8bdc2c5db773f805409f4998
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-12-11 11:51:42 -0800

    created view for homepage

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "title should be reddit's title" do
+    get 'home'
+    assert_select "title", "reddit: the front page of the internet"
+  end
 end
