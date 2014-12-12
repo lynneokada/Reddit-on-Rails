c13c0f88668e63ac629a6704bafde52036825393
Morgan Chen <morganchen12@gmail.com>
2014-12-12 11:39:49 -0800

    update form css, did something to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    @user.save
+    redirect_to root_url
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
