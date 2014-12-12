313410241877aca2958925ff58d87a936bb1491b
lokada <lyokada@ucsc.edu>
2014-12-11 18:51:02 -0800

    backtrace_silencer, guard initialized

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
 
