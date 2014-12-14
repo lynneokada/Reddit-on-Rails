670c06a45ad598bb5d47ccc5ce28acc81773855c
Morgan Chen <morganchen12@gmail.com>
2014-12-13 16:21:23 -0800

    added sessions without cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by(id: user_id)
+    elsif false # remove false and check for cookies later
+      # handle cookies later
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
 
   # validations
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,6 +22,17 @@ class User < ActiveRecord::Base
     return SecureRandom.urlsafe_base64
   end
 
+  def
+
   # instance methods
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
 end
