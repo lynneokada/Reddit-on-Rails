c6f9ec01fb881df5d660d7f813f5a5fca0018e5b
lokada <lyokada@ucsc.edu>
2014-12-14 17:40:23 -0800

    redditpost_form displayed in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def home
     @user = User.new
+    @redditpost = current_user.redditposts.build if logged_in?
+    @redditposts = Redditpost.all
   end
 end
