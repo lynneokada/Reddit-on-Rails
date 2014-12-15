7dc5371f8512d4a6b39ee3c2c1419a1eb2f19a71
lokada <lyokada@ucsc.edu>
2014-12-15 14:59:56 -0800

    redditpost.test modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214103145) do
+ActiveRecord::Schema.define(version: 20141215221717) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "redditposts", force: true do |t|
     t.string   "title"
--- test/models/redditpost_test.rb
+++ test/models/redditpost_test.rb
@@ -4,7 +4,7 @@ class RedditpostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:lynne)
-    @redditpost = @user.redditposts.build(content: "https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/522085_10103964458375665_7692157870835212098_n.jpg?oh=6263ca0032abe7642b0583255aa3f4cc&oe=554031E8&__gda__=1426146896_6361cc3d3da259d6b644bb03a9dd0371")
+    @redditpost = @user.redditposts.build(title: "CSYES", content: "https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/522085_10103964458375665_7692157870835212098_n.jpg?oh=6263ca0032abe7642b0583255aa3f4cc&oe=554031E8&__gda__=1426146896_6361cc3d3da259d6b644bb03a9dd0371")
   end
 
   test "reddit post should be valid" do
@@ -20,8 +20,4 @@ class RedditpostTest < ActiveSupport::TestCase
     @redditpost.content = "      "
     assert_not @redditpost.valid?
   end
-
-  # test "order should be most recent first" do
-  #   assert_equal Redditpost.first, redditposts(:most_recent)
-  # end
 end
