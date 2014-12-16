7ed7d9764625fa5d5836247485508168dcb46cb7
Morgan Chen <morganchen12@gmail.com>
2014-12-16 15:21:09 -0800

    added comment form to redditpost view, will update with ajax later

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,15 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
 
   validates :up, presence: true
-  validates :user_id, presence: true
-  validate :post_xor_comment
-
-  private
-
-    def post_xor_comment
-      if !(post_id.blank? ^ comment_id.blank?)
-        errors.add(:base, "Vote can be on a comment or post, but not both")
-      end
-    end
-
+  validates :votable_id, presence: true
+  validates :votable_type, presence: true
 end
