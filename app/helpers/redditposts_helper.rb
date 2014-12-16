75d0baed3e493a4618e5d7cc740a94440a6085ef
Morgan Chen <morganchen12@gmail.com>
2014-12-15 16:53:00 -0800

    added posts to front page

--- app/helpers/redditposts_helper.rb
+++ app/helpers/redditposts_helper.rb
@@ -4,4 +4,10 @@ module RedditpostsHelper
     noun.pluralize(count)
   end
 
+  def pluralize_without_count(count, noun, text = nil)
+    if count != 0
+      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+    end
+  end
+
 end
