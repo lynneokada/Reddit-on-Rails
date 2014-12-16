db5ee2332d004ce173d7d1a352c3aa987326cf95
Morgan Chen <morganchen12@gmail.com>
2014-12-16 12:28:17 -0800

    added validations that I forgot earlier

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :redditpost
   belongs_to :user
   has_many :votes, as: :votable, dependent: :destroy
+
+  validates :redditpost_id, presence: true
+  validates :body, presence: true           # actual reddit allows blank comments
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20141216191352) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "comment_id"
   end
 
   add_index "redditposts", ["user_id", "created_at"], name: "index_redditposts_on_user_id_and_created_at"
