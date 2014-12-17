02cc4f705d0662761f5afff97b2a2a9885c8db6d
lokada <lyokada@ucsc.edu>
2014-12-17 14:27:42 -0800

    vote buttons are being displayed with filled in params

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :redditposts, only: [:create, :destroy]
+  resources :votes, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
