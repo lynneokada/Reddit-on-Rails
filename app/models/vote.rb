f54ef6c5c971939b4413b6fe673cdf892e88f73d
Morgan Chen <morganchen12@gmail.com>
2014-12-17 14:16:12 -0800

    added uniqueness validation to vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,4 +5,5 @@ class Vote < ActiveRecord::Base
   validates :up, presence: true
   validates :votable_id, presence: true
   validates :votable_type, presence: true
+  validates :user_id, uniqueness: {scope: [:votable_type, :votable_id]}
 end
