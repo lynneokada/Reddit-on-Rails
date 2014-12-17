065cf42eb74903d0fa46736915ad760af9e6c782
Luke Solomon <lukesolomon@Lukes-MacBook-Pro.local>
2014-12-17 14:11:59 -0800

    Ready for merge, votes unfinished

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,4 +7,16 @@ class VotesController < ApplicationController
   def destroy
 
   end
+
+
+  def post 
+
+  end	
+
+  private
+
+  def votable_params(params_object = params[:voteType])
+    params_object.permit(:votable_id, :votable_type, :user_id, )
+  end
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,21 @@
+#reguire
+
+#module
+
 class Vote < ActiveRecord::Base
+
+
   belongs_to :user
+  belongs_to :comment
   belongs_to :votable, polymorphic: true
+
+  scope :up, lambda{ where(:vote_flag => true) }
+  scope :down, lambda{ where(:vote_flag => false) }
 
   validates :up, presence: true
   validates :votable_id, presence: true
   validates :votable_type, presence: true
+  
+  
+  
 end
