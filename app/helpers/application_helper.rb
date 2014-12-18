f38bef8b28d75b673aaa4e6c223c78bb7756b3aa
lokada <lyokada@ucsc.edu>
2014-12-18 13:33:47 -0800

    test for sorting posts and comments passing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,23 @@
 module ApplicationHelper
 
   def score_for_votable(votes, object_created_at, gravity = 1.8)
+    if !votes
+      raise "votes cannot be nil"
+    end
     object_age = ((Time.zone.now - object_created_at) / 3600).round
     return (votes - 1) / ((object_age + 2) ** gravity)
+  end
+
+  def sort_score_for_votable(votable)
+    votable.sorting_score = score_for_votable(votable.score, votable.created_at)
+  end
+
+  def update_all_sorting_scores
+    Redditpost.all.each do |redditpost|
+      sort_score_for_votable(redditpost)
+    end
+    Comment.all.each do |comment|
+      sort_score_for_votable(comment)
+    end
   end
 end
--- test/models/redditpost_test.rb
+++ test/models/redditpost_test.rb
@@ -4,7 +4,6 @@ class RedditpostTest < ActiveSupport::TestCase
   def setup
     @user = users(:lynne)
     @redditpost = redditposts(:morgante)
-    # @redditpost = @user.redditposts.build(title: "CSYES", content: "https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/522085_10103964458375665_7692157870835212098_n.jpg?oh=6263ca0032abe7642b0583255aa3f4cc&oe=554031E8&__gda__=1426146896_6361cc3d3da259d6b644bb03a9dd0371")
   end
 
   test "reddit post should be valid" do
@@ -24,5 +23,18 @@ class RedditpostTest < ActiveSupport::TestCase
   test "should return votable score of redditpost" do
     sorting_score = score_for_votable(@redditpost.score, @redditpost.created_at)
     assert sorting_score > 0
+  end
+
+  test "should sort redditpost by scoring order" do
+    # byebug
+    update_all_sorting_scores
+    ordered_by_sorting_score = Redditpost.order(sorting_score: :DESC)
+    ordered_by_date = Redditpost.order(created_at: :DESC)
+    length = ordered_by_date.count-1
+    # byebug
+    (0..length).each do |i|
+      assert ordered_by_sorting_score[i].title == ordered_by_date[i].title
+    end
+    # assert_equal(ordered_by_date, ordered_by_sorting_score, "sorting post score failed")
   end
 end
