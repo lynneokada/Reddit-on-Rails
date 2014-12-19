970e396b04085b480e88610706feeab6285e802d
lokada <lyokada@ucsc.edu>
2014-12-19 15:04:52 -0800

    ajax for votes to redditposts and comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = current_redditpost.comments.build(user_id: current_user.id, body: params[:comment][:content])
+    comment.score = 0
     comment.save
     redirect_to current_redditpost
   end
@@ -12,7 +13,7 @@ class CommentsController < ApplicationController
 
   private
 
-    def current_redditpost 
+    def current_redditpost
       Redditpost.find(params[:id])
     end
 
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,15 @@
 class VotesController < ApplicationController
+  include ApplicationHelper
 
   def create
-    vote = current_user.votes.build(user_id: params[:user_id],
+    @vote = current_user.votes.build(user_id: params[:user_id],
                                     votable_id: params[:votable_id],
                                     votable_type: params[:votable_type],
                                     up: params[:up])
-    vote.score++
-    vote.save
+
+    @vote.save
+    @redditpost = Redditpost.find(@vote.votable_id)
+    @redditpost.update_score
     respond_to do |format|
       format.html { redirect_to root_url }
       format.js
