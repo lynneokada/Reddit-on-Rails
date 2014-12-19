11a8dea6318313e7f211f1290d1b213bc76a7a83
lokada <lyokada@ucsc.edu>
2014-12-19 11:38:59 -0800

    idk what I'm doing

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,24 @@
 class VotesController < ApplicationController
 
   def create
-    vote = current_user.votes.build(user_id: params[:user_id], 
-                                    votable_id: params[:votable_id],  
-                                    votable_type: params[:votable_type], 
+    vote = current_user.votes.build(user_id: params[:user_id],
+                                    votable_id: params[:votable_id],
+                                    votable_type: params[:votable_type],
                                     up: params[:up])
+    vote.score++
     vote.save
-    redirect_to root_url
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.js
+    end
   end
 
   def destroy
-
+    # vote = Vote.find(params[:id]).votable_type
+    # current_user.unvote(vote)
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.js
+    end
   end
 end
