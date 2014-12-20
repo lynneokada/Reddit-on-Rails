5ea51d3931cb3cc0e2cb39e2c21b011e1eb16f3b
lokada <lyokada@ucsc.edu>
2014-12-19 16:54:55 -0800

    subreddit form to home, redditpost form to subreddit view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def home
-    @user = current_user || User.new
-    @redditpost = current_user.redditposts.build if logged_in?
+    # @redditpost = current_user.redditposts.build if logged_in?
     @redditposts = Redditpost.order(sorting_score: :DESC)
+    @subreddit = Subreddit.new
   end
 end
